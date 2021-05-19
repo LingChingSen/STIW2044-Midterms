423b718d206afee695969fe876056043db779e52
LingChingSen <61204151+LingChingSen@users.noreply.github.com>
2021-05-20 01:28:27 +0800

    Add files via upload

---
+++ lib/newproduct.dart
@@ -0,0 +1,386 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:image_picker/image_picker.dart';
+import 'package:midtermstiw2044myshop/main.dart';
+import 'package:progress_dialog/progress_dialog.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:image_cropper/image_cropper.dart';
+
+class AddNewProduct extends StatefulWidget {
+  @override
+  _AddNewProductState createState() => _AddNewProductState();
+}
+
+class _AddNewProductState extends State<AddNewProduct> {
+  double screenHeight, screenWidth;
+  File _image;
+  String pathAsset = 'assets/images/nopic.png';
+  ProgressDialog pr;
+  TextEditingController _nameController = new TextEditingController();
+  TextEditingController _typeController = new TextEditingController();
+  TextEditingController _priceController = new TextEditingController();
+  TextEditingController _qtyController = new TextEditingController();
+
+  @override
+   Widget build(BuildContext context) {
+    screenHeight = MediaQuery.of(context).size.height;
+    screenWidth = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('New Product'),
+      ),
+      body: Center(
+        child: Container(
+          child: Padding(
+              padding: EdgeInsets.fromLTRB(30, 10, 30, 10),
+              child: SingleChildScrollView(
+                  child: Column(
+                children: [
+                  GestureDetector(
+                    onTap: () => {_onPictureSelectionDialog()},
+                    child: Container(
+                        height: screenHeight / 3.2,
+                        width: screenWidth / 2,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: _image == null
+                                ? AssetImage(pathAsset)
+                                : FileImage(_image),
+                            fit: BoxFit.scaleDown,
+                          ),
+                          border: Border.all(
+                            width: 3.0,
+                            color: Colors.grey,
+                          ),
+                          borderRadius: BorderRadius.all(Radius.circular(
+                                  10.0) 
+                              ),
+                        )),
+                  ),
+                  SizedBox(height: 5),
+                  Text("Click to add product image",
+                      style: TextStyle(fontSize: 10.0, color: Colors.black)),
+            
+                  TextField(
+                    controller: _nameController,
+                    keyboardType: TextInputType.name,
+                    decoration: InputDecoration(labelText: 'Name'),
+                  ),
+                  TextField(
+                    controller: _typeController,
+                    decoration: InputDecoration(labelText: 'Type'),
+                  ),
+                  TextField(
+                    controller: _priceController,
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(labelText: 'Price (RM)'),
+                  ),
+                  TextField(
+                    controller: _qtyController,
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(labelText: 'Quantity'),
+                  ),
+                  SizedBox(height: 15),
+                  MaterialButton(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      minWidth: screenWidth,
+                      height: 50,
+                      child: Text('Add New Product',
+                          style: TextStyle(
+                            color: Colors.white,
+                          )),
+                      onPressed: () {
+                        postNewProductDialog();
+                      },
+                      color: Colors.blue),
+                  SizedBox(height: 10),
+                ],
+              ))),
+        ),
+      ),
+    );
+  }
+
+  _onPictureSelectionDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+            
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(10.0))),
+            content: new Container(
+             
+              height: screenHeight / 4,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                      alignment: Alignment.center,
+                      child: Text(
+                        "Take picture from:",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16),
+                      )),
+                  SizedBox(height: 5),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Flexible(
+                          child: MaterialButton(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(5.0)),
+                        minWidth: 100,
+                        height: 100,
+                        child: Text('Camera',
+                            style: TextStyle(
+                              color: Colors.white,
+                            )),
+                       
+                        color: Theme.of(context).accentColor,
+                        elevation: 10,
+                        onPressed: () =>
+                            {Navigator.pop(context), _chooseCamera()},
+                      )),
+                      SizedBox(width: 10),
+                      Flexible(
+                          child: MaterialButton(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(5.0)),
+                        minWidth: 100,
+                        height: 100,
+                        child: Text('Gallery',
+                            style: TextStyle(
+                              color: Colors.white,
+                            )),
+                       
+                        color: Theme.of(context).accentColor,
+                        elevation: 10,
+                        onPressed: () =>
+                            {Navigator.pop(context), _chooseGallery()},
+                      )),
+                    ],
+                  ),
+                ],
+              ),
+            ));
+      },
+    );
+  }
+
+  Future _chooseCamera() async {
+    final picker = ImagePicker();
+    final pickedFile = await picker.getImage(
+      source: ImageSource.camera,
+      maxHeight: 800,
+      maxWidth: 800,
+    );
+
+    if (pickedFile != null) {
+      _image = File(pickedFile.path);
+    } else {
+      print('No image selected.');
+    }
+
+    _cropImage();
+  }
+
+  _chooseGallery() async {
+    final picker = ImagePicker();
+    final pickedFile = await picker.getImage(
+      source: ImageSource.gallery,
+      maxHeight: 800,
+      maxWidth: 800,
+    );
+
+    if (pickedFile != null) {
+      _image = File(pickedFile.path);
+    } else {
+      print('No image selected.');
+    }
+
+    _cropImage();
+  }
+
+  _cropImage() async {
+    File croppedFile = await ImageCropper.cropImage(
+        sourcePath: _image.path,
+        aspectRatioPresets: [
+          CropAspectRatioPreset.square,
+        ],
+        androidUiSettings: AndroidUiSettings(
+            toolbarTitle: 'Crop your image',
+            toolbarColor: Colors.red,
+            toolbarWidgetColor: Colors.white,
+            initAspectRatio: CropAspectRatioPreset.original,
+            lockAspectRatio: true),
+        iosUiSettings: IOSUiSettings(
+          minimumAspectRatio: 1.0,
+        ));
+
+    if (croppedFile != null) {
+      _image = croppedFile;
+      setState(() {});
+    }
+  }
+
+  void postNewProductDialog() {
+   String _name = _nameController.text.toString();
+    String _type = _typeController.text.toString();
+    String _price = _priceController.text.toString();
+    
+
+    if(_name.isEmpty && _type.isEmpty && _price.isEmpty  ){
+      Fluttertoast.showToast(
+        msg: "Please fill in all textfield",
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.blue,
+        textColor: Colors.white,
+        fontSize: 16.0);
+      return;
+    }
+    else if(_name.isEmpty ){
+      Fluttertoast.showToast(
+        msg: "Name is empty",
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.blue,
+        textColor: Colors.white,
+        fontSize: 16.0);
+      return;
+    }else if(_type.isEmpty ){
+      Fluttertoast.showToast(
+        msg: "Type is empty",
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.blue,
+        textColor: Colors.white,
+        fontSize: 16.0);
+      return;
+    }else if(_price.isEmpty){
+      Fluttertoast.showToast(
+        msg: "Price is empty",
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.blue,
+        textColor: Colors.white,
+        fontSize: 16.0);
+      return;
+   
+    }else if(_image == null){
+      Fluttertoast.showToast(
+        msg: "Please insert image",
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.blue,
+        textColor: Colors.white,
+        fontSize: 16.0);
+      return;
+    }
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(20.0))),
+            title: Text("Add New Product???"),
+            content: Text("Are your sure?"),
+            actions: [
+              TextButton(
+                child: Text("Ok"),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                  _addNewProduct();
+                },
+              ),
+              TextButton(
+                  child: Text("Cancel"),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  }),
+            ],
+          );
+        });
+  }
+
+  
+  Future<void> _addNewProduct() async {
+    pr = ProgressDialog(context);
+    pr.style(
+      message: 'Posting...',
+      borderRadius: 5.0,
+      backgroundColor: Colors.white,
+      progressWidget: CircularProgressIndicator(),
+      elevation: 10.0,
+      insetAnimCurve: Curves.easeInOut,
+    );
+    pr = ProgressDialog(context,
+        type: ProgressDialogType.Normal, isDismissible: true, showLogs: true);
+        await pr.show();
+        String base64Image = base64Encode(_image.readAsBytesSync());
+        String name = _nameController.text.toString();
+        String type = _typeController.text.toString();
+        String price = _priceController.text.toString();
+        String qty = _qtyController.text.toString();
+        print(name);
+        http.post(
+          Uri.parse("https://crimsonwebs.com/s271738/myshop/php/newproduct.php"),
+          body: {
+            "name": name,
+            "type": type,
+            "price": price,
+            "qty": qty,
+            "encoded_string": base64Image
+        }).then((response) {
+            pr.hide().then((isHidden) {
+            print(isHidden);
+      });
+      print(response.body);
+      if (response.body == "success") {
+        Fluttertoast.showToast(
+            msg: "Success",
+            toastLength: Toast.LENGTH_SHORT,
+            gravity: ToastGravity.BOTTOM,
+            timeInSecForIosWeb: 1,
+            backgroundColor: Colors.red,
+            textColor: Colors.white,
+            fontSize: 16.0);
+        setState(() {
+          _image = null;
+          _nameController.text = "";
+          _typeController.text = "";
+          _priceController.text = "";
+          _qtyController.text = "";
+        });
+         Navigator.push(
+        context, MaterialPageRoute(builder: (content) =>MyApp()));
+      
+      } else {
+        Fluttertoast.showToast(
+            msg: "Failed",
+            toastLength: Toast.LENGTH_SHORT,
+            gravity: ToastGravity.BOTTOM,
+            timeInSecForIosWeb: 1,
+            backgroundColor: Colors.red,
+            textColor: Colors.white,
+            fontSize: 16.0);
+      }
+    });
+  }
+}
